fb0216c91e1161472ab4a77b4bc1e17aca5511b4
Rails Udemy Course <railsudemycourse@gmail.com>
2016-05-18 06:01:29 +0000

    add edit update to suers and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,22 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to articles_path
     else
-      rendre "new"
+      render "new"
     end
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was successfully updated"
+      redirect_to articles_path
+    else
+      render "new"
+    end
   end
 
   private
